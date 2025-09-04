4eebf5e0848eb448f036f54eb4b2b216cf1ccec0
Sue Nguyen <suenguyen.guthib@gmail.com>
2025-09-04 00:23:17 -0400

    exercise 1.3

--- src/chapter01/Exercise101.java
+++ src/chapter01/Exercise101.java
@@ -1,6 +1,6 @@
 package chapter01;
 
-public class Exercises101 {
+public class Exercise101 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
         System.out.println("Welcome to Computer Science");
