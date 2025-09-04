b2722aa5dd83238ee03fecabf0422db9f478fdfb
Sue Nguyen <suenguyen.guthib@gmail.com>
2025-09-04 00:04:52 -0400

    exercise 1.1

---
+++ src/chapter01/Exercises101.java
@@ -0,0 +1,9 @@
+package chapter01;
+
+public class Exercises101 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
